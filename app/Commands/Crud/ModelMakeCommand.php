c403495d296670f385477a7a2d718fa5dbdab6ca
sarthaksavvy <bitfumes@gmail.com>
2019-10-26 14:43:19 +0530

    build for resource command on pull request

--- app/Commands/Crud/ModelMakeCommand.php
+++ app/Commands/Crud/ModelMakeCommand.php
@@ -38,7 +38,7 @@ class ModelMakeCommand extends GeneratorCommand
      */
     public function handle()
     {
-        if (parent::handle() === false && ! $this->option('force')) {
+        if (parent::handle() === false && !$this->option('force')) {
             return false;
         }
 
